7a9a9f858356d46777139cb8860dd06fd66dfdad
Faqih Arifian <faqih.arifian@go-jek.com>
2021-07-22 13:07:33 +0700

    sleep in range

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
     "encoding/json"
     "io/ioutil"
+    "math/rand"
     "net/http"
     "time"
 )
 
 func ping(w http.ResponseWriter, req *http.Request) {
     defer req.Body.Close()
-    sleepDuration := 0.0
+    sleepDuration := 0
     requestBody := map[string]interface{}{}
     if req.Body != nil {
         bodyBytes, _ := ioutil.ReadAll(req.Body)
@@ -19,7 +20,7 @@ func ping(w http.ResponseWriter, req *http.Request) {
             return
         }
         var ok bool
-        sleepDuration, ok = requestBody["sleep_duration"].(float64)
+        sleepDuration, ok = getSleepDuration(requestBody)
         if !ok {
             w.WriteHeader(http.StatusBadRequest)
             return
@@ -30,6 +31,23 @@ func ping(w http.ResponseWriter, req *http.Request) {
     w.Write([]byte("pong"))
     w.WriteHeader(http.StatusOK)
     return
+}
+
+func getSleepDuration(requestBody map[string]interface{}) (int, bool) {
+    sleepDuration, ok := requestBody["sleep_duration"].(float64)
+    if ok {
+        return int(sleepDuration), true
+    }
+    sleepFrom, ok := requestBody["sleep_from"].(float64)
+    if !ok {
+        return 0, false
+    }
+    sleepTo, ok := requestBody["sleep_to"].(float64)
+    if !ok {
+        return 0, false
+    }
+    duration := rand.Intn(int(sleepTo) - int(sleepFrom)) + int(sleepFrom)
+    return duration, true
 }
 
 func main() {
